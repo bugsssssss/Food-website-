c4cb40571e4a643f13aadd785b057806eae05ea7
Nurbek <93489667+bugsssssss@users.noreply.github.com>
2022-08-10 11:15:20 +0500

    new

---
+++ js/script.js
@@ -0,0 +1,12 @@
+window.addEventListener('scroll', function() {
+    let header = document.querySelector('header')
+    header.classList.toggle('sticky', window.scrollY > 0)
+})
+
+function toggleMenu () {
+    let menuToggle = document.querySelector('.menuToggle')
+    let navigation = document.querySelector('.navigation')
+    menuToggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
+
